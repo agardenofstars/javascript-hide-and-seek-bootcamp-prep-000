f645f0716c3c10cdf737167c69c833cae40ba508
ljubica berger <ljubicaberger@gmail.com>
2019-01-01 06:18:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+function getFirstSelector(selector) {
+  
+  var firstElement = document.querySelectorAll(':first-child');
+  
+  return firstElement;
+  
+}
 
+function nestedTarget() {
+  
+  var nestedElement = document.querySelectorAll('#nested .target');
+  
+  return nestedElement;
+  
+}
+
+function increaseRankBy(n){
+  
+  const rankedLists = document.querySelectorAll('.ranked-list');
+  
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    
+    let children = rankedLists[i].children;
+    
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  
+  }
+
+}
+
+
+function deepestChild() {
+  
+  const deeplyNested = document.querySelectorAll('div#grand-node');
+  
+  
+}

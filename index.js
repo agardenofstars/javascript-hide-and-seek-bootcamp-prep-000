3c109736796cad493133c3b59f2a2345de37edc2
ljubica berger <ljubicaberger@gmail.com>
2019-01-01 06:19:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,18 +18,8 @@ function increaseRankBy(n){
   
   const rankedLists = document.querySelectorAll('.ranked-list');
   
-  for (let i = 0, l = rankedLists.length; i < l; i++) {
-    
-    let children = rankedLists[i].children;
-    
-    for (let j = 0, k = children.length; j < k; j++) {
-      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
-    }
-  
-  }
-  
   for (let i = 0; i < rankedLists.length; i++) {
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
   }
 
 }

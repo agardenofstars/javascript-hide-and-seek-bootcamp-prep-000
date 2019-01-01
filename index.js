8e3ee945e46741cac22344069a8e2d970cce43ad
ljubica berger <ljubicaberger@gmail.com>
2019-01-01 06:27:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector) {
+  
+  return document.querySelector(selector);
+  
+}
 
+function nestedTarget() {
+  
+  return document.getElementById('nested').querySelector('.target');
+  
+}
+
+function increaseRankBy(n){
+  
+  const rankedLists = document.querySelectorAll('.ranked-list');
+  
+  for (let i = 0; i < rankedLists.length; i++) {
+    rankedLists[i].innerHTML = parseInt(rankedLists[i].innerHTML) + n;
+  }
+
+}
+
+
+function deepestChild() {
+  let deeplyNested = document.querySelectorAll('#grand-node div');
+  return deeplyNested[deeplyNested.length - 1];
+}
